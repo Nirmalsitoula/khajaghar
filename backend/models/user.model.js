9f10e562ecc8268d46f642f9f985a90468234a50
Nirmalsitoula <nirmalsitoula97@gmail.com>
2026-07-24 20:53:59 +0545

    User schema

---
+++ backend/models/user.model.js
@@ -0,0 +1,27 @@
+const userSchema=new mongoose.Schema({
+    fullName:{
+        type : String,
+        required:true
+    },
+    email:{
+         type : String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type: String
+    },
+    mobile:{
+        type : String ,
+        required: true
+
+    },
+    role:{
+        type:String,
+        enum:["user" , "owner" , "deleveryBoy"],
+        required:true
+    }
+},{timestamps:true})
+
+const User=mongoose.model("User",userSchema)
+export default User
